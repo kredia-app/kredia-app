641aff35f510e1053a39f3cfa181195e2724f026
dori871992 <dorianmusaj@gmail.com>
2025-11-08 15:13:38 +0100

    Update base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/kredia-app", // Replace with your actual repo name
+  basePath: "", // Replace with your actual repo name
   images: {
     unoptimized: true,
   },
