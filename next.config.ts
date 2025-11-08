600b448ccc9c67d43aeacf4f15441649933f5949
dori871992 <dorianmusaj@gmail.com>
2025-11-08 13:37:51 +0100

    include out

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '', // Replace with your actual repo name
+  output: "export",
+  basePath: "kredia-app", // Replace with your actual repo name
   images: {
     unoptimized: true,
   },
